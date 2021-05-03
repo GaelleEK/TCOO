0f57555bf7e49bce9ee2b00a281f15e349058d85
gaelleEK <elkadaouigaelle@gmail.com>
2021-05-03 20:06:46 +0200

    💄 [style] add images assets and style about

--- src/views/About.vue
+++ src/views/About.vue
@@ -7,19 +7,33 @@
       </p>
       <p class="content has-text-centered pt-5"></p>
       <div class="level">
-        <div class="level-item box has-background-white p-0 m-0">Saisissez une adresse</div>
-        <div class="level-item is-capitalized is-size-5">OU</div>
-        <div class="level-item box has-background-white p-0 m-0">Uploadez un fichier</div>
+        <div class="level-item box has-background-white has-text-centered">
+          <div class="">
+            <div class="heading">Saisissez une adresse</div>
+            <div class="content">
+              <figure class="image is-256*256">
+                <img src="../assets/images/input-adresse.jpeg">
+              </figure>
+            </div>
+          </div>
+        </div>
+        <div class="level-item is-capitalized is-size-5">
+          <div>OU</div>
+        </div>
+        <div class="level-item box has-background-white has-text-centered">
+          <div class="">
+            <div class="heading">Uploadez un fichier</div>
+            <div class="content">
+              <figure class="image is-256*256">
+                <img src="../assets/images/input-upload-adresse.jpeg">
+              </figure>
+            </div>
+          </div>
+        </div>
       </div>
       <div class="level">
-        <div class="level-item box has-background-white p-0 m-0">Cliquez pour demander les coordonnées</div>
+        <div class="level-item box has-background-white has-text-centered">Cliquez pour demander les coordonnées</div>
       </div>
     </div>
-    
-
-
   </div>
 </template>
-
-
-
